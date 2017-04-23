f33f8acf797a9ee6077e875d674da1067874ae74
Vitaly Tomilov <vitaly.tomilov@gmail.com>
2017-04-23 15:07:13 +0100

    changing to classes + docs.

--- test/databaseSpec.js
+++ test/databaseSpec.js
@@ -5,20 +5,19 @@ var lib = require('../');
 describe("Database", function () {
 
     it("must initialize correctly", function () {
-        expect(lib.Database({}) instanceof lib.Database).toBe(true);
         expect(new lib.Database({}) instanceof lib.Database).toBe(true);
     });
 
     it("must throw on invalid parameters", function () {
         var error = new TypeError('Invalid driver specified.');
         expect(function () {
-            lib.Database(undefined);
+            new lib.Database(undefined);
         }).toThrow(error);
         expect(function () {
-            lib.Database(123);
+            new lib.Database(123);
         }).toThrow(error);
         expect(function () {
-            lib.Database('hello');
+            new lib.Database('hello');
         }).toThrow(error);
     });
 
